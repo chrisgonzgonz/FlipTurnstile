d73dbdcab6927b9ed015b65ed9ec983ec9e5d2f1
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-02-14 20:18:08 -0500

    adds GNZSplitsTableViewController

---
+++ FlipTurnstile/GNZSplitsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GNZSplitsTableViewController.h
+//  FlipTurnstile
+//
+//  Created by Chris Gonzales on 2/10/15.
+//  Copyright (c) 2015 GNZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GNZSplitsTableViewController : NSObject
+
+@end
---
+++ FlipTurnstile/GNZSplitsTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  GNZSplitsTableViewController.m
+//  FlipTurnstile
+//
+//  Created by Chris Gonzales on 2/10/15.
+//  Copyright (c) 2015 GNZ. All rights reserved.
+//
+
+#import "GNZSplitsTableViewController.h"
+
+@implementation GNZSplitsTableViewController
+
+@end
