0e86173ef0924a2f181b0f5df4d1025e19e11817
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-02-15 19:09:10 -0500

    adds GNZRace model

---
+++ FlipTurnstile/GNZRace.h
@@ -0,0 +1,15 @@
+//
+//  GNZRace.h
+//  FlipTurnstile
+//
+//  Created by Chris Gonzales on 2/15/15.
+//  Copyright (c) 2015 GNZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class GNZSwimmer;
+@interface GNZRace : NSObject
+@property (nonatomic) GNZSwimmer *swimmer;
+@property (nonatomic, readonly) NSArray *laps;
+@end
---
+++ FlipTurnstile/GNZRace.m
@@ -0,0 +1,16 @@
+//
+//  GNZRace.m
+//  FlipTurnstile
+//
+//  Created by Chris Gonzales on 2/15/15.
+//  Copyright (c) 2015 GNZ. All rights reserved.
+//
+
+#import "GNZRace.h"
+
+@interface GNZRace ()
+@property (nonatomic, readwrite) NSArray *laps;
+@end
+@implementation GNZRace
+
+@end
