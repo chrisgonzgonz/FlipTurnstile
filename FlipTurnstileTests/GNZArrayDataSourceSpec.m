5afb8a66502101bf4e173f8d0065a48760c95d32
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-02-22 19:25:38 -0500

    removes unused dependencies

--- FlipTurnstileTests/GNZArrayDataSourceSpec.m
+++ FlipTurnstileTests/GNZArrayDataSourceSpec.m
@@ -10,8 +10,6 @@
 #import "GNZArrayDataSource.h"
 #define EXP_SHORTHAND
 #import <Expecta.h>
-#define HC_SHORTHAND
-#import <OCHamcrest/OCHamcrest.h>
 #define MOCKITO_SHORTHAND
 #import <OCMockito/OCMockito.h>
 
